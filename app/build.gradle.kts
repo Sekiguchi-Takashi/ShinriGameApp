54041cf03ba78aa88243bbc0fbde0e8f08b42b61
Sekiguchi-Takashi <Sekiguchi-Takashi@users.noreply.github.com>
2026-08-16 01:32:14 +0900

    v1.3.1 upload-artifact削除

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.appathy.shinrigame"
         minSdk = 24
         targetSdk = 34
-        versionCode = 17
-        versionName = "1.3"
+        versionCode = 18
+        versionName = "1.3.1"
     }
 
     buildTypes {
